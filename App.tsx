8cde1ba50aab366aa618c5b3c11015076a811b6d
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2024-02-21 19:34:48 -0300

    App created

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
+import 'react-native-gesture-handler';
+import { AppRoutes } from '@/routes/AppRoutes';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <AppRoutes />
+      <StatusBar style="light" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
